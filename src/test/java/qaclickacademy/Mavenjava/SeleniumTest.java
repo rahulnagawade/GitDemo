c2f2f3bd45715edb24bbf5bc36e5f72f7ac31eab
rahul <rahul.nagawade555@gmail.com>
2020-07-27 12:42:43 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/SeleniumTest.java
@@ -0,0 +1,20 @@
+//Maven project is created and framework is explained without actual code just to check how it works
+
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomation");
+	}
+	@Test
+	public void elementUi()
+	{
+		System.out.println("elementUi");
+	}
+	
+}
